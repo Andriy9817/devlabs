2a0df7d60a44886207580c2a5b2b8782f69e4323
Andriy9817 <andriybalian98@gmail.com>
2019-11-10 23:02:59 +0200

    Lab_3 : finished lab

--- lab_3/main/views.py
+++ lab_3/main/views.py
@@ -1,3 +1,17 @@
+import platform
+from datetime import datetime
+
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def main(request):
+    return render(request, 'main.html', {'parameter': "test"})
+
+
+def health(request):
+    response = {'date': datetime.now(), 'current_page': request.path,
+                'server_info': {'system': platform.system(), 'node': platform.node(), 'release': platform.release(),
+                                'version': platform.version(), 'machine': platform.machine(), 'processor': platform.processor()},
+                'client_info': request.headers.get('User-Agent')}
+    return JsonResponse(response)
